7a3fa0e990350a58e47e0f1410ca9e17e82f0604
Dhananjay Bhatkar <dhananjay.bhatkar999@outlook.com>
2024-04-24 19:24:03 +0530

    Tip Calculator using Array and Bill variable

--- script.js
+++ script.js
@@ -246,7 +246,7 @@ console.log(friends.includes(25));
 
 // Coding Challenges Part Two Array----------------------------------------------------------------------------
 
-
+/*
 const calcTip = (bill) => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
 const bill = [125, 555, 44];
@@ -255,5 +255,8 @@ const tips = [calcTip(bill[0]), calcTip(bill[1]), calcTip(bill[2])];
 
 const total = [bill[0] + tips[0], bill[1] + tips[1], bill[2] + tips[2]];
 
-console.log(bill, tips, total);
+console.log("Bills:- " + bill, " Tips:- " + tips, " Totals:- " +  total);
+
+*/
+
 
